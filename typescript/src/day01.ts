c07251b472df885eeda3e12d6990d6b9c39c11bd
coryg <coryg2178@gmail.com>
2022-12-23 20:49:25 -0500

    feat: day 1 part 2 solution

--- typescript/src/day01.ts
+++ typescript/src/day01.ts
@@ -18,5 +18,34 @@ fs.readFile(`./src/inputs/day01.txt`, "utf8", (err, data) => {
     largest = max(largest, sum);
   });
 
-  console.log(largest);
+  console.log(`Part 1 solution: ${largest}`);
+});
+
+// Part 2
+fs.readFile(`./src/inputs/day01.txt`, "utf8", (err, data) => {
+  if (err) throw err;
+
+  // Convert data to a 2d array of numbers
+  const dataArr = data.split("\n\n").map((i) => i.split("\n").map(Number));
+  // Top three array
+  const topThree = [0, 0, 0];
+
+  dataArr.forEach((arr) => {
+    // Get the sum of all items in the array
+    let sum = arr.reduce((current, i) => current + i, 0);
+
+    // Update the top three
+    topThree.forEach((item, idx) => {
+      let tmp = 0;
+      if (sum > item) {
+        tmp = item;
+        topThree[idx] = sum;
+        sum = tmp;
+      }
+    });
+  });
+
+  console.log(
+    `Part 2 solution: ${topThree.reduce((current, i) => current + i, 0)}`
+  );
 });
