7176ef66b75a32b001a5196fe243b83c4a12ec9a
coryg2178 <coryg2178@gmail.com>
2022-12-24 15:55:26 -0500

    feat: day 2 part 2 solution

--- typescript/src/day02.ts
+++ typescript/src/day02.ts
@@ -11,9 +11,13 @@ interface strategy {
 fs.readFile(`./src/inputs/day02.txt`, "utf8", (err, data) => {
   if (err) throw err;
 
-  const arr = data.split("\n").map((i) => i.split(" "));
+  // Strategy Guide
+  const sg = data
+    .trim()
+    .split("\n")
+    .map((i) => i.split(" "));
 
-  // Part 1
+  // Part 1 -> X is rock, Y is paper, Z is scissors
   const p1: strategy = {
     A: { X: 4, Y: 8, Z: 3 },
     B: { X: 1, Y: 5, Z: 9 },
@@ -21,6 +25,17 @@ fs.readFile(`./src/inputs/day02.txt`, "utf8", (err, data) => {
   };
 
   console.log(
-    `Part 1 solution: ${arr.reduce((acc, i) => acc + p1[i[0]][i[1]], 0)}`
+    `Part 1 solution: ${sg.reduce((acc, i) => acc + p1[i[0]][i[1]], 0)}`
+  );
+
+  // Part 2 -> X is lose, Y is draw, Z is win
+  const p2: strategy = {
+    A: { X: 3, Y: 4, Z: 8 },
+    B: { X: 1, Y: 5, Z: 9 },
+    C: { X: 2, Y: 6, Z: 7 },
+  };
+
+  console.log(
+    `Part 1 solution: ${sg.reduce((acc, i) => acc + p2[i[0]][i[1]], 0)}`
   );
 });
