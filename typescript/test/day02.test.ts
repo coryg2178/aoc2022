f30ba3cd193a53de0e8363acf0a9e2c40e82cf31
coryg <coryg2178@gmail.com>
2022-12-26 18:43:34 -0500

    feat: day02 testing

---
+++ typescript/test/day02.test.ts
@@ -0,0 +1,12 @@
+import { partOne, partTwo } from "../src/day02";
+import { readInput } from "../src/utils";
+
+const data = readInput("./test/inputs/day02.txt");
+
+test("Day 1 Part 1", () => {
+  expect(partOne(data)).toBe(15);
+});
+
+test("Day 1 Part 2", () => {
+  expect(partTwo(data)).toBe(12);
+});
