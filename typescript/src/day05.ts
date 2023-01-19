4c50b7a6bef0d087c9ed8d0dbb5d9c02be06a162
coryg2178 <coryg2178@gmail.com>
2023-01-19 15:19:57 -0500

    feat: day 5 part 2

--- typescript/src/day05.ts
+++ typescript/src/day05.ts
@@ -32,27 +32,44 @@ export const getMoves = (data: string) => {
 };
 
 export const partOne = (crates: string[][], moves: number[][]) => {
-  for (let i = 0; i < moves.length; i++) {
-    const instr = moves[i];
-    for (let j = 0; j < instr[0]; j++) {
-      const n = crates[instr[1] - 1].pop() || "";
-      crates[instr[2] - 1].push(n);
+  moves.forEach((m) => {
+    const numMoves = m[0];
+    const startStk = m[1] - 1;
+    const endStk = m[2] - 1;
+    for (let j = 0; j < numMoves; j++) {
+      crates[endStk].push(crates[startStk].pop() || "");
     }
-  }
+  });
 
   return crates.map((s) => s.at(-1)).join("");
 };
 
-export const partTwo = (data: string) => {
-  return "";
+export const partTwo = (crates: string[][], moves: number[][]) => {
+  moves.forEach((m) => {
+    const numMoves = -m[0];
+    const startStk = m[1] - 1;
+    const endStk = m[2] - 1;
+
+    crates[endStk]?.push(...crates[startStk]?.slice(numMoves));
+    crates[startStk]?.splice(numMoves);
+  });
+
+  return crates.map((s) => s.at(-1)).join("");
 };
 
 export const dayFive = () => {
   const input = readInput("./src/inputs/day05.txt");
 
-  const crates = getCrates(input.split("\n\n")[0]);
-  const moves = getMoves(input.split("\n\n")[1]);
-
-  console.log(`Part 1 solution: ${partOne(crates, moves)}`);
-  console.log(`Part 2 solution: ${partTwo(input)}`);
+  console.log(
+    `Part 1 solution: ${partOne(
+      getCrates(input.split("\n\n")[0]),
+      getMoves(input.split("\n\n")[1])
+    )}`
+  );
+  console.log(
+    `Part 2 solution: ${partTwo(
+      getCrates(input.split("\n\n")[0]),
+      getMoves(input.split("\n\n")[1])
+    )}`
+  );
 };
